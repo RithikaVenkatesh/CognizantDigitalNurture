11c81c08918cf332bae3637fbfbca1c4ae1d26ee
Rithika Venkatesh <rithikav2004@gmail.com>
2025-06-30 00:26:21 +0530

    Saving local work before merging

---
+++ DSA/FinancialForecastApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FinancialForecastApp
+{
+    class Program
+    {
+        // Recursive method to predict future value
+        static double PredictFutureValue(double initialAmount, double rate, int years)
+        {
+            if (years == 0)
+                return initialAmount;
+
+            return (1 + rate) * PredictFutureValue(initialAmount, rate, years - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter initial amount:");
+            double initial = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter annual growth rate (as percentage):");
+            double percent = Convert.ToDouble(Console.ReadLine());
+            double rate = percent / 100.0;
+
+            Console.WriteLine("Enter number of years:");
+            int years = Convert.ToInt32(Console.ReadLine());
+
+            double futureValue = PredictFutureValue(initial, rate, years);
+            Console.WriteLine($"\nPredicted future value after {years} years: ₹{futureValue:F2}");
+        }
+    }
+}
---
+++ Design pattern/SingletonPatternExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log("This is the first log message.");
+            logger2.Log("This is the second log message.");
+
+            if (logger1 == logger2)
+            {
+                Console.WriteLine("Both logger1 and logger2 are the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances exist.");
+            }
+        }
+    }
+}
